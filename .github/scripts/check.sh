7b247766ac338b5715b591622ce5af95efa0389a
dapopovic <anazorex.bus@gmail.com>
2025-07-28 16:48:41 +0200

    Refactor check.sh for improved readability and consistency in command formatting

--- .github/scripts/check.sh
+++ .github/scripts/check.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-./gradlew ktlintCheck 2>&1 | grep '\.kt:' | sed 's/\([^:]*\.kt:[0-9]*:[0-9]*\) \(.*\)/\1 Error: \2/' | reviewdog  \
--name="ktlint" \
--reporter=github-pr-check \
--efm="%f:%l:%c %t: %m" \
--level=error \
--filter-mode=nofilter \
--fail-level=error \
+./gradlew ktlintCheck 2>&1 | \
+  grep '\.kt:' | \
+  sed 's/\([^:]*\.kt:[0-9]*:[0-9]*\) \(.*\)/\1 E: \2/' | \
+  reviewdog \
+    -name="ktlint" \
+    -reporter=github-pr-check \
+    -efm="%f:%l:%c %t: %m" \
+    -level=error \
+    -filter-mode=nofilter \
+    -fail-level=error
