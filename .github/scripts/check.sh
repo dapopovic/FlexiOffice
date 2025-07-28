5e141f0e455b85f6e850f0c2db76eda4e13d66e8
dapopovic <anazorex.bus@gmail.com>
2025-07-28 16:28:41 +0200

    Update check.sh to use ktlintCheck and improve error formatting

--- .github/scripts/check.sh
+++ .github/scripts/check.sh
@@ -1,7 +1,8 @@
-./gradlew lintKotlin 2>&1 | grep 'Lint error > ' | sed 's/ Lint error > /e: /g' | reviewdog  \
+#!/bin/bash
+./gradlew ktlintCheck 2>&1 | grep '\.kt:' | sed 's/\([^:]*\.kt:[0-9]*:[0-9]*\) \(.*\)/\1 Error: \2/' | reviewdog  \
 -name="ktlint" \
 -reporter=github-pr-check \
--efm="%f:%l:%c:%t: %m" \
+-efm="%f:%l:%c %t: %m" \
 -level=error \
 -filter-mode=nofilter \
 -fail-level=error \
