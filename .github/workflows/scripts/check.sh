1355c965e9f7cfd5f99d43b10c18e79ec85bd7a4
dapopovic <anazorex.bus@gmail.com>
2025-07-28 15:55:44 +0200

    Add check.sh script for Kotlin linting with reviewdog

---
+++ .github/workflows/scripts/check.sh
@@ -0,0 +1,7 @@
+./gradlew lintKotlin 2>&1 | grep 'Lint error > ' | sed 's/ Lint error > /e: /g' | reviewdog  \
+-name="ktlint" \
+-reporter=github-pr-review \
+-efm="%f:%l:%c:%t: %m" \
+-level=error \
+-filter-mode=nofilter \
+-fail-on-error
